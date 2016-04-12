c56dfe60500e817f45bc7f638fe00d0ccf73b0fa
parakawa <paty.arakawa@gmail.com>
2016-04-12 17:16:50 -0500

    aprendiendo a deletear

--- array6.rb
+++ array6.rb
@@ -1,10 +1,9 @@
 #aprendiendo a pushear
  
 puts "Ingrese números separados por un espacio"
-numeros = gets.chomp.split(" ")
+x = gets.chomp.split(" ")
 w=["solucion"]
-n=numeros.length
-while x!=null do
+while x!=nil do
 		x.delete_at(0)
 end
 print x
